4fa53775e0341a4710b6d4407c97816f070cdf1f
Abdul-Azeez <zeezu124@gmail.com>
2023-05-19 16:28:30 +0100

    modified tests for new routes

--- test.py
+++ test.py
@@ -4,22 +4,42 @@ import requests
 
 class FlaskTest(unittest.TestCase):
     def setUp(self):
-        self.url = 'http://localhost:5000'
-        self.query = 'example query'
+        self.url = 'http://127.0.0.1:5000/results'
+        self.query = 'I hate everything lol'
         self.data = {
-            'search_query': self.query
+            'input_text': self.query
             }
 
-    def test_search(self):
+    def test_results(self):
         # make a post request to your Flask app
-        data = self.data
-        response = requests.post(url=self.url, data=json.dumps(data))
+        response = requests.post(url=self.url, json=self.data)
 
-        # Print the response from the Flask application
-        print(response.content.decode('utf-8'))
+        # Convert the response content to JSON
+        json_data = response.json()
 
-        # assert that the response is of type object
-        self.assertIsInstance(response, object)
+        # Make assertions on the JSON data
+        
+        assert isinstance(json_data, dict) #Expected JSON response to be a dictionary
+        assert "output" in json_data #Expected 'output' key to be present in JSON response
+        #Expected 'key' to have value specified
+        assert json_data["output"] == "The text: I hate everything lol | denotes this emotion: annoyance | with a confidence score of 0.9987181425094604"
+        
+    def test_results_error(self):
+            # make a post request to your Flask app
+            data = {'input_not_text': ''}
+            response = requests.post(url=self.url, json=data)
+
+            # Convert the response content to JSON
+            json_data = response.json()
+
+            # Make assertions on the JSON data
+            print(json_data)
+            assert isinstance(json_data, dict) #Expected JSON response to be a dictionary
+            assert "error" in json_data #Expected 'error' key to be present in JSON response
+            #Expected 'key' to have value specified
+            assert json_data["error"] == "Invalid request"
+        
+
 
 if __name__ == '__main__':
     unittest.main()
